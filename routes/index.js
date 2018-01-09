397b5a56414997cdb9dc365e05f1385c5410a2a8
Shengmei Bai <1023638368@qq.com>
2018-01-09 15:14:09 +0800

    实现分页功能

--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,26 @@ var crypto = require('crypto'),
 
 module.exports = function(app) {
   app.get('/', function (req, res) {
-    Post.getAll(null, function (err, posts) {
+    //判断是否是第一页，并把请求的页数转换成 number 类型
+    var page = req.query.p ? parseInt(req.query.p) : 1; // 这里req.query.p获取到的页数为字符串形式
+    //查询并返回第 page 页的 10 篇文章
+    Post.getTen(null, page, function (err, posts, total) {
       if (err) {
         posts = [];
       } 
       res.render('index', {
         title: '主页',
-        user: req.session.user,
         posts: posts,
+        page: page,
+        isFirstPage: (page - 1) == 0,
+        isLastPage: ((page - 1) * 10 + posts.length) == total,
+        user: req.session.user,
         success: req.flash('success').toString(),
         error: req.flash('error').toString()
       });
     });
   });
+  
   app.get('/reg', checkNotLogin);
   app.get('/reg', function (req, res) {
     res.render('reg', {
@@ -145,14 +152,15 @@ module.exports = function(app) {
   });
 
   app.get('/u/:name', function (req, res) {
+    var page = req.query.p ? parseInt(req.query.p) : 1;
     //检查用户是否存在
     User.get(req.params.name, function (err, user) {
       if (!user) {
         req.flash('error', '用户不存在!'); 
-        return res.redirect('/');//用户不存在则跳转到主页
+        return res.redirect('/');
       }
-      //查询并返回该用户的所有文章
-      Post.getAll(user.name, function (err, posts) {
+      //查询并返回该用户第 page 页的 10 篇文章
+      Post.getTen(user.name, page, function (err, posts, total) {
         if (err) {
           req.flash('error', err); 
           return res.redirect('/');
@@ -160,9 +168,12 @@ module.exports = function(app) {
         res.render('user', {
           title: user.name,
           posts: posts,
-          user : req.session.user,
-          success : req.flash('success').toString(),
-          error : req.flash('error').toString()
+          page: page,
+          isFirstPage: (page - 1) == 0,
+          isLastPage: ((page - 1) * 10 + posts.length) == total,
+          user: req.session.user,
+          success: req.flash('success').toString(),
+          error: req.flash('error').toString()
         });
       });
     }); 
